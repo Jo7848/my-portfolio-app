446585741f757486bd00e736d1b5d8a453acfb87
Jo7848 <indiatsiwilliams@gmail.com>
2025-08-13 07:04:58 -0700

    Add new project to projects section

--- src/data/projects.js
+++ src/data/projects.js
@@ -20,8 +20,18 @@ const projects = [
     githubLink: 'https://github.com/Jo7848/my-portfolio-app' // Updated GitHub link
   },
   // Add more projects here following the same structure!
+   {
+    id: 3,
+    title: "vitalforge-website",
+   image: "/images/vitalforge-website.jpg",
+    description: "Vitalforge: A professional and responsive single-page application built with React to showcase a personal trainer's services and expertise. It features a dynamic user interface with smooth page transitions and a sleek, mobile-first design for an optimal user experience.",
+    techs: ["React", "Vite", "HTML", "CSS", "JavaScript", "Responsive Design"],
+     liveLink: "https://vitalforge-website.vercel.app/",
+     githubLink: "https://github.com/Jo7848/vitalforge-website"
+   }
+   // Add more projects here following the same structure!
   // {
-  //   id: 3,
+  //   id: 4,
   //   title: "Another Project",
   //   image: "/images/another-project-thumbnail.jpg",
   //   description: "Description for another project...",
